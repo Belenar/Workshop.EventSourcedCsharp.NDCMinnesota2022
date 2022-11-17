56ef5b6159845601339c748de2f01eac7b13cadc
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-17 18:15:57 +0100

    A couple of value type

---
+++ src/BeerSender/BeerSender.Domain/Value_types.cs
@@ -0,0 +1,17 @@
+﻿namespace BeerSender.Domain;
+
+public record Shipping_label(string Shipping_code, Carrier Carrier) {
+    public bool IsValid()
+    {
+        return Shipping_code.Length > 12;
+    }
+}
+
+public enum Carrier
+{
+    UPS,
+    FedEx,
+    DHL
+}
+
+public record Beer_bottle(string Brewery, string Name);
---
+++ src/BeerSender/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using BeerSender.Domain;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var label1 = new Shipping_label("123456", Carrier.DHL);
+            var label2 = new Shipping_label("123456", Carrier.DHL);
+
+            Console.WriteLine(label1);
+
+            Console.WriteLine(label1 == label2);
+
+            var label3 = label1 with { Shipping_code = "234567" };
+
+            Console.WriteLine(label3);
+        }
+    }
+}
