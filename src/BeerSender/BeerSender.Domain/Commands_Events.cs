917364faf2d3ec4c2090a808b12b22ba38c09970
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-17 12:02:08 -0600

    Command router and test base

---
+++ src/BeerSender/BeerSender.Domain/Commands_Events.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BeerSender.Domain;
+
+// Commands
+public record Create_package(Guid Package_id);
+public record Add_beer(Guid package_id, Beer_bottle beer);
+
+// Events
+public record Package_created(Guid Package_id);
+public record Beer_added(Guid package_id, Beer_bottle beer);
+public record Beer_failed_to_add(Guid package_id, Beer_bottle beer, Fail_reason reason)
+
+public enum Fail_reason
+{
+    Box_was_full
+}
