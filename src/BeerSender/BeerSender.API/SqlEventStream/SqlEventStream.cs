4bf1371d4eae0db0f39572554e524e998790cc66
Grant Witte <grant.witte@orthofi.com>
2022-11-17 16:57:32 -0600

    select event out of eventStream

--- src/BeerSender/BeerSender.API/SqlEventStream/SqlEventStream.cs
+++ src/BeerSender/BeerSender.API/SqlEventStream/SqlEventStream.cs
@@ -14,6 +14,7 @@ public class SqlEventStream
         return _eventContext.PersistedEvent
             .Where(e => e.AggregateId == aggregateId)
             .OrderBy(e => e.CreatedOn)
+            .Select(e => e.Event)
             .ToList();
     }
 
