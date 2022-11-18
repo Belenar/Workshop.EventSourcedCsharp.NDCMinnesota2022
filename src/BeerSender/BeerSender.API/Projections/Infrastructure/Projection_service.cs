97dda320cafc92c3b4bd81323b1b6e99385820bd
Hannes Lowette <lowette.hannes@gmail.com>
2022-11-18 18:22:09 +0100

    Missing file again :(

--- src/BeerSender/BeerSender.API/Projections/Infrastructure/Projection_service.cs
+++ src/BeerSender/BeerSender.API/Projections/Infrastructure/Projection_service.cs
@@ -1,6 +1,16 @@
 ﻿namespace BeerSender.API.Projections.Infrastructure
 {
-    public class Projection_service
+    public class Projection_service<TProjection> : BackgroundService
+        where TProjection : class, IProjection
     {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Fetch a batch
+            // Run the projection for said batch
+            // Commit the batch
+            // Update checkpoint
+            // (wait)
+            // Repeat
+        }
     }
 }
